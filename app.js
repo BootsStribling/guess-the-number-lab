838e1a5ec8561d46371ebd47769ad6f5ec149e43
BootsStribling <williamstriblingenterprises@gmail.com>
2022-01-30 20:08:08 -0600

    add getGuess feature

--- app.js
+++ app.js
@@ -7,5 +7,24 @@ const game = {
   secretNum: null,
   play: function() {
     this.secreNum = Math.floor(Math.random() * (this.biggestNum-this.smallestNum + 1)) + this.smallestNum
+  },
+  prevGuesses: []
+}
+
+let playerGuess = 55;
+
+function getGuess() {
+  smallestNum = game.smallestNum;
+  biggestNum = game.biggestNum;
+  console.log(`Enter a guess between ${smallestNum} and ${biggestNum}.`)
+  if(typeof playerGuess === 'number' && playerGuess < biggestNum && playerGuess > smallestNum){
+    game.prevGuesses.push(playerGuess);
+  }
+  if(typeof playerGuess !== 'number'){
+    console.log('Please enter a number.')
+  }
+  if(playerGuess > biggestNum || playerGuess < smallestNum){
+    console.log(`Please enter a number between ${smallestNum} and ${biggestNum}.`)
   }
 }
+getGuess();
